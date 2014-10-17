2c1a7ff5b37362e95f4a2870f8ea6c5cc5724aab
Louis Bouchard <louis.bouchard@canonical.com>
2014-10-17 16:34:10 +0200

    Implement configuration file modification
    Do sanity checks and modify the configuration file according
    to the running tests
    
    Signed-off-by: Louis Bouchard <louis.bouchard@canonical.com>

--- kdump-test-script.py
+++ kdump-test-script.py
@@ -6,6 +6,8 @@ from time import time, localtime
 test_phase = ['local-only', 'local', 'ssh', 'nfs']
 _EBAD = -1
 _crash_dir = '/var/crash'
+_ssh_remote_server = 'ubuntu@kdump-netcrash'
+_nfs_remote_mountpoint = 'kdump-netcrash:/var/crash'
 
 #
 # Used to indicate that netdump tests
@@ -39,12 +41,40 @@ def create_ref_conf():
 
 
 def set_conffile(test):
-    if test == 'ssh':
-        pass
-    elif test == 'nfs':
-        pass
-    else:
-        raise TypeError("Invalid test")
+    with open("/etc/default/kdump-tools.ref", "r") as orig:
+        try:
+            if test == 'local' or test == 'local-only':
+                with open("/etc/default/kdump-tools", "w") as new_conf:
+                    for line in orig.readlines():
+                        if line.find('USE_KDUMP') == 0:
+                            new_conf.write("{}".format('USE_KDUMP=1\n'))
+                        else:
+                            new_conf.write("{}".format(line))
+            elif test == 'ssh':
+                ref = orig.read()
+                if ref.find("SSH") == 0:
+                    with open("/etc/default/kdump-tools", "w") as new_conf:
+                        for line in orig.readlines():
+                            new_conf.write("{}".format(line))
+                        new_conf.write("{}".format(_ssh_remote_server))
+                else:
+                    print("SSH functionality not found in {}".format('/etc/default/kdump-tools'))
+                    return _EBAD
+            elif test == 'nfs':
+                ref = orig.read()
+                if ref.find("NFS") == 0:
+                    with open("/etc/default/kdump-tools", "w") as new_conf:
+                        for line in orig.readlines():
+                            new_conf.write("{}".format(line))
+                        new_conf.write("{}".format(_nfs_mountpoint))
+                else:
+                    print("NFS functionality not found in {}".format('/etc/default/kdump-tools'))
+                    return _EBAD
+            else:
+                raise TypeError("Invalid test")
+        except PermissionError as err:
+            print("User has not the required privilege to update the file\t{}".format(err))
+            return _EBAD
     return
 
 
@@ -111,7 +141,14 @@ if __name__ == '__main__':
             phase = 'local'
     print("Running phase {}".format(phase.upper()))
     if phase != 'completed':
-        run_test(phase)
+        if set_conffile(phase) != _EBAD:
+            run_test(phase)
+        else:
+            print("Unable to continue with tests")
+            f = open('/var/crash/next-test', 'w')
+            f.write('completed\n')
+            f.close()
+            exit(_EBAD)
     else:
         os.unlink('/var/crash/next-test')
 
