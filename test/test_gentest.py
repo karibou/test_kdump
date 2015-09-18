d37e1662ae49a162a5a7791a3ffa7045ad4d4328
Louis Bouchard <louis.bouchard@canonical.com>
2015-09-18 18:06:10 +0200

    [test] Add new test coverage
    Signed-off-by: Louis Bouchard <louis.bouchard@canonical.com>

--- test/test_gentest.py
+++ test/test_gentest.py
@@ -16,4 +16,31 @@ class gentestTests(unittest.TestCase):
     def tearDownClass(letest):
         shutil.rmtree(letest.workdir)
 
+    def test_render_exists(self):
+        self.target = os.path.join(self.workdir, 'test-kdump')
+        # create pre-existing file
+        with open(self.target, 'w') as target:
+            target.write('a')
 
+        self.assertEqual(gentest.render('test-kdump', self.target,
+                         self.context), 1)
+        output = sys.stdout.getvalue().strip()
+        self.assertEqual(output, 'File %s already exists' % self.target)
+
+    def test_render_exists_with_force(self):
+        self.target = os.path.join(self.workdir, 'test-kdump')
+        # create pre-existing file
+        with open(self.target, 'w') as target:
+            target.write('a')
+
+        self.context['force'] = True
+        self.assertEqual(gentest.render('test-kdump', self.target,
+                         self.context), None)
+
+    def test_render_with_filename(self):
+
+        self.context['output_file'] = 'testfile'
+        self.target = os.path.join(self.workdir, 'testfile')
+        self.assertEqual(gentest.render('test-kdump', self.target,
+                         self.context), None)
+        self.assertTrue(os.path.exists(self.target))
